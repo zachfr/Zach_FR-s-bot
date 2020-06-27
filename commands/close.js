79eac88489254f52828c290ee09e1a98a4be4008
zachfr <42076517+zachfr@users.noreply.github.com>
2020-06-26 23:13:04 -0400

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -2,8 +2,9 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let supportrole = message.member.roles.cache.has('725913350755844106');
-    if (message.channel.parent === 'Tickets') {
-        message.channel.send('Please enter `confirm` to close ticket! You have 10 seconds').then(() => {
+    if (supportrole) {
+        if (message.chanel.parent === 'Tickets') {
+            message.channel.send('Please enter `confirm` to close ticket! You have 10 seconds').then(() => {
             const filter = m => message.author.id === m.author.id;
         
             message.channel.awaitMessages(filter, { time: 10000, max: 1, errors: ['time'] })
@@ -17,7 +18,10 @@ module.exports.run = async (bot, message, args) => {
                 .catch(() => {
                     message.channel.send("You don't enter `confirm` to close ticket! Please retype command `!close`");
                 });
-        });
+            });
+        } else {
+            message.channel.send("You need do this command in ticket");
+        }
     } else {
         message.channel.send("You don't have permission to do that");
     }
