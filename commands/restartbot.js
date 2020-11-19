20a9037acc47358b08029217e2191415560781a2
Zach_FR <zac232003@icloud.com>
2020-11-18 21:21:03 -0500

    Change message of place

--- commands/restartbot.js
+++ commands/restartbot.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    message.channel.send("I restart");
     setTimeout(() => {
         if (message.author.id === '277242592801062913') {
-            message.channel.send("I restart");
             process.exit();
         }
     }, 1000)
