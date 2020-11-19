ff8a2ba5db15520f47b940cacaaca49abfb70527
Zach_FR <zac232003@icloud.com>
2020-11-18 20:49:13 -0500

    Pull command (beta)

---
+++ commands/pull.js
@@ -0,0 +1,14 @@
+﻿const Discord = require("discord.js");
+const simpleGit = require('simple-git');
+const git = simpleGit();
+
+module.exports.run = async (bot, message, args) => {
+    if(message.author.id !== "277242592801062913") return;
+    git.pull();
+}
+
+module.exports.help = {
+    name: "pull",
+    description: "Update bot to last file on github"
+}
+module.exports.aliases = []
