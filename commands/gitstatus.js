0fe946a884d8a1dd8f8ce6d75ec4167bf6e6a40c
Zach_FR <zac232003@icloud.com>
2020-11-18 21:07:42 -0500

    Update command

--- commands/gitstatus.js
+++ commands/gitstatus.js
@@ -4,7 +4,7 @@ const git = simpleGit();
 
 module.exports.run = async (bot, message, args) => {
     if(message.author.id !== "277242592801062913") return;
-    console.log(git.status);
+    console.log(git.status(args));
 }
 
 module.exports.help = {
