d67e6b295178cafb0c7f6f6e909d43d473f21b5e
Zach_FR <zac232003@icloud.com>
2020-11-19 10:40:50 -0500

    Catch errors?

--- commands/pull.js
+++ commands/pull.js
@@ -4,8 +4,13 @@ const git = simpleGit();
 
 module.exports.run = async (bot, message, args) => {
     if(message.author.id !== "277242592801062913") return;
-    git.pull();
-    message.channel.send("I succesfull update my file to last commit. You problably need restart me to apply change.");
+    try {
+        git.pull();
+        message.channel.send("I succesfull update my file to last commit. You problably need restart me to apply change.");
+    } catch (error) {
+        message.channel.send("Errors check in log.");
+        console.log(error);
+    }
 }
 
 module.exports.help = {
